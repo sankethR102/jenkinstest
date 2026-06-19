088de9dd18a849e1bacd976a59def644de82873f
sankethR102 <sanketh.ravishankar@gmail.com>
2026-06-19 12:48:33 +0530

    modified app  again

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Student(db.Model):
 
 @app.route("/")
 def home():
-    return "Flask + PostgreSQL + Docker Compose 🚀 version 2"
+    return "Flask + PostgreSQL + Docker Compose 🚀 version 3 heyyy!!!!!!!"
 
 
 @app.route("/add/<name>")
